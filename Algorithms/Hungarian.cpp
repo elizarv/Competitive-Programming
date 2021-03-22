96788272fdc6cc3c397970680b2a812144bd5073
José Manuel Salazar <manuelsalazar1030@gmail.com>
2021-03-22 17:01:34 -0500

    Update Hungarian.cpp

--- Algorithms/Hungarian.cpp
+++ Algorithms/Hungarian.cpp
@@ -1,38 +1,41 @@
+Maximum weighted bipartite matching (min cost)
 
-//Maximum bipartite matching
+typedef ll T;
+const T inf = 1e18;
 
-typedef ll th;
-const th INF = 1e18;
-struct Hung{
+struct hung {
     int n, m;
-    vector<th> u, v; vector<int> p, way;
-    vector<vector<th>> a;
-    Hung(int n, int m):
-        n(n), m(m), a(vector<vector<th>>(n+1, vector<th>(m+1, INF-1))),
-        u(n+1), v(m+1), p(m+1), way(m+1){}
-        void set(int x, int y, th v){ a[x+1][y+1] = v; }
-        th assign(){
-            forr(i, 1, n+1){
-                int j0 = 0; p[0] = i;
-                vector<th> minv(m+1, INF);
-                vector<char> used(m+1, false);
-                do{
-                    used[j0] = true;
-                    int i0 = p[j0], j1; th delta = INF;
-                    forr(j, 1, m+1)if(!used[j]){
-                        th cur = a[i0][j]-u[i0]-v[j];
-                        if(cur<minv[j])minv[j] = cur, way[j] = j0;
-                        if(minv[j] < delta)delta = minv[j], j1 = j;
-                    }
-                    forn(j, m+1)
-                        if(used[j])u[p[j]] += delta, v[j] -= delta;
-                        else minv[j] -= delta;
-                    j0 = j1;
-                }while(p[j0]);
-                do{
-                    int j1 = way[j0]; p[j0] = p[j1]; j0 = j1;
-                }while(j0);
-            }
-            return -v[0];
+    vector<T> u, v; vector<int> p, way;
+    vector<vector<T>> g;
+    
+    hung(int n, int m):
+        n(n), m(m), g(n+1, vector<T>(m+1, inf-1)),
+        u(n+1), v(m+1), p(m+1), way(m+1) {}
+        
+    void set(int u, int v, T w) { g[u+1][v+1] = w; }
+    
+    T assign() {
+        for (int i = 1; i <= n; ++i) {
+            int j0 = 0; p[0] = i;
+            vector<T> minv(m+1, inf);
+            vector<char> used(m+1, false);
+            do {
+                used[j0] = true;
+                int i0 = p[j0], j1; T delta = inf;
+                for (int j = 1; j <= m; ++j) if (!used[j]) {
+                    T cur = g[i0][j] - u[i0] - v[j];
+                    if (cur < minv[j]) minv[j] = cur, way[j] = j0;
+                    if (minv[j] < delta) delta = minv[j], j1 = j;
+                }
+                for (int j = 0; j <= m; ++j)
+                    if (used[j]) u[p[j]] += delta, v[j] -= delta;
+                    else minv[j] -= delta;
+                j0 = j1;
+            } while (p[j0]);
+            do {
+                int j1 = way[j0]; p[j0] = p[j1]; j0 = j1;
+            } while (j0);
         }
+        return -v[0];
+    }
 };
